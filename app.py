3f2009715594de29f4e1a005c147b28ecbeaa069
Tanmay Mishra <tanmaymishra.129@gmail.com>
2026-08-15 14:14:03 +0530

    Initial commit - SMS spam classifier

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+stop_words = set(stopwords.words('english'))
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stop_words and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+
+# Load model and vectorizer
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+# Streamlit UI
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+
+if st.button("Predict"):
+
+    transformed_sms = transform_text(input_sms)
+
+    vector_input = tfidf.transform([transformed_sms])
+
+    result = model.predict(vector_input.toarray())[0]
+
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
